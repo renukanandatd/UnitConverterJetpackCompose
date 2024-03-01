2ba81caf110b5bc9446e5530bf0ef80863f9af32
renukananda_td <renukananda2000@gmail.com>
2024-03-01 10:35:26 +0530

    Final commit

--- app/src/main/java/com/example/unitconverterjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/unitconverterjetpackcompose/MainActivity.kt
@@ -22,12 +22,17 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverterjetpackcompose.ui.theme.UnitConverterJetpackComposeTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,6 +54,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter(){
 
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    val outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember { mutableStateOf(1.00) }
+    val oConversionFactor = remember { mutableStateOf(1.00) }
+
+    fun convertUnit(){
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
+        val result = (inputValueDouble*conversionFactor.value*100/oConversionFactor.value).roundToInt()/100.0
+        outputValue = result.toString()
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -56,51 +76,90 @@ fun UnitConverter(){
     ) {
         Text(text = "Unit Converter App")
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange ={})
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange ={
+            inputValue = it
+        },
+            label = {
+                Text(text = "Enter value")
+            })
         Spacer(modifier = Modifier.height(16.dp))
        Row {
            Box {
-               Button(onClick = { /*TODO*/ }) {
+               Button(onClick = { iExpanded=true}) {
                    Text(text = "SELECT")
                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                }
-               DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+               DropdownMenu(expanded = iExpanded, onDismissRequest = { iExpanded=false }) {
                    DropdownMenuItem(text = {
                        Text(text = "Centimeters" )},
-                       onClick = { /*TODO*/ }
+                       onClick = {
+                           iExpanded = false
+                           inputUnit = "Centimeters"
+                           conversionFactor.value = 0.01
+                           convertUnit()
+                       }
                    )
                    DropdownMenuItem(text = {
                        Text(text = "Meters" )},
-                       onClick = { /*TODO*/ }
+                       onClick = {
+                           iExpanded = false
+                           inputUnit = "Meters"
+                           conversionFactor.value = 1.0
+                           convertUnit()
+                       }
                    )
                    DropdownMenuItem(text = {
                        Text(text = "Feet" )},
-                       onClick = { /*TODO*/ }
+                       onClick = {
+                           iExpanded = false
+                           inputUnit = "Feet"
+                           conversionFactor.value = 0.3048
+                           convertUnit()
+                       }
                    )
                }
            }
            Spacer(modifier = Modifier.width(16.dp))
            Box {
-               Button(onClick = { /*TODO*/ }) {
+               Button(onClick = { oExpanded=true }) {
                    Text(text = "SELECT")
                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                }
-               DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+               DropdownMenu(expanded = oExpanded, onDismissRequest = { oExpanded = false}) {
                    DropdownMenuItem(text = {
                        Text(text = "Centimeters" )},
-                       onClick = { /*TODO*/ }
+                       onClick = {
+                           oExpanded = false
+                           inputUnit = "Centimeters"
+                           conversionFactor.value = 0.01
+                           convertUnit()
+                       }
                    )
                    DropdownMenuItem(text = {
                        Text(text = "Meters" )},
-                       onClick = { /*TODO*/ }
+                       onClick = {
+                           oExpanded = false
+                           inputUnit = "Meters"
+                           conversionFactor.value = 1.0
+                           convertUnit()
+                       }
                    )
                    DropdownMenuItem(text = {
                        Text(text = "Feet" )},
-                       onClick = { /*TODO*/ }
+                       onClick = {
+                           oExpanded = false
+                           inputUnit = "Feet"
+                           conversionFactor.value = 0.3048
+                           convertUnit()
+                       }
                    )
                }
            }
        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "RESULT ${outputValue}")
     }
 }
 @Preview(showBackground = true)
